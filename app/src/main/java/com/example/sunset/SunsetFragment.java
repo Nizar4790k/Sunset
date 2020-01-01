ab867803d4289da81c1d0a155b0a551d45011111
Nizar <nizar4790k@gmail.com>
2019-12-31 21:40:19 -0400

    Figure 32.2

---
+++ app/src/main/java/com/example/sunset/SunsetFragment.java
@@ -0,0 +1,56 @@
+package com.example.sunset;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SunsetFragment extends Fragment {
+
+
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
+    public static SunsetFragment newInstance(){
+        return new SunsetFragment();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
+
+        return view;
+    }
+
+
+    private void startAnimation() {
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd).setDuration(3000);
+
+        heightAnimator.start();
+
+    }
+
+
+
+}
